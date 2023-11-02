fdd9a65dcd20e36d6adca3ee5cf867e890f3cbad
vasilva <vinicius.alex89@gmail.com>
2023-11-02 11:25:04 -0300

    test

--- tests.py
+++ tests.py
@@ -1,6 +1,11 @@
 import unittest
+from lembrete import Calendario
 
-      
+class TestCalendario(unittest.TestCase):
+    def test_event(self):
+        cal = Calendario(2023, 2)
+        cal.add_evento(12, "Evento")
+        self.assertEqual(cal.mes, 2)
 
 if __name__ == "__main__":
     unittest.main()
